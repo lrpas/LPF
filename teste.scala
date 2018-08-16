5f1c70a616b550711c09560033f2f503a8d256e3
larissa silva <larissasilva@polijuniorengenharia.com.br>
2018-08-16 17:31:57 -0300

    primeiro_push

---
+++ teste.scala
@@ -0,0 +1,25 @@
+def soma (x:Int,y:Int):Int = {
+  
+  return x+y;
+  
+}
+
+println(soma(10,20));
+
+def ehPar(z:Int):Boolean={
+  if(z%2==0)
+  return true;
+  else
+  return false;
+  
+}
+
+def ehImpar(w:Int)={
+  if(w%2 !=0)
+  println("eh impar");
+  else
+  println("eh par");
+}
+
+println(ehPar(10));
+ehImpar(13);
