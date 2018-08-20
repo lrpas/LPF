7716aa69c1bbf34c8e9aeb1b91edc3272d095d7f
larissa silva <larissasilva@polijuniorengenharia.com.br>
2018-08-20 15:18:01 -0300

    classe_heranca

--- classe.scala
+++ classe.scala
@@ -1,24 +1,3 @@
-object larissa{
-  
-  val nome: String = "Larissa";
-  var idade = 21;
-  val curso = "Ecomp";
-  def aniversario={
-    
-    idade=idade+1;
-      
-    }
-  
-  def imprime=(nome + " tem "+ idade +" e cursa: "+curso);
-  
-}
-
-------------
-
-println(larissa.imprime);
-larissa.aniversario;
-println(larissa.imprime);
-
 class pessoa(_nome:String,_idade:Int){
   
   val nome:String = _nome;
@@ -28,10 +7,19 @@ class pessoa(_nome:String,_idade:Int){
     idade = idade+1;
   }
   
-  
-  
+  def imprime() = nome + " tem " + idade + " anos";
 }
+  
+
 
 var larissa = new pessoa ("larissa",21);
 println(larissa.nome + larissa.idade);
 
+class estudante (nome:String,idade:Int,curso:String,_periodo:Int) extends pessoa (nome,idade){
+  var periodo:Int= _periodo;
+  override def imprime = nome + " tem " + idade + "e cursa " + curso + " no " + periodo +" periodo " ;
+  
+}
+
+var silva = new estudante ("larissa regina ",21,"ECOMP",5);
+println(silva.imprime);
